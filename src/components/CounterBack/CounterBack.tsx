ec7a16da0a19772c31799e4858cca0dcf3b8e89b
Gabriel Andres <gabrielleoandres@gmail.com>
2022-08-22 15:43:07 +0200

    Fixed corrections with bem and the div

--- src/components/CounterBack/CounterBack.tsx
+++ src/components/CounterBack/CounterBack.tsx
@@ -1,25 +1,23 @@
 const CounterBack = (): JSX.Element => {
   return (
-    <div className="counter-container">
-      <ul className="counter-list">
-        <li className="days item">
-          <span className="days number">02</span>
-          <span className="days words">days</span>
-        </li>
-        <li className="hours item">
-          <span className="hours number">20</span>
-          <span className="hours word">hours</span>
-        </li>
-        <li className="minutes item">
-          <span className="minutes number">34</span>
-          <span className="minutes word">minutes</span>
-        </li>
-        <li className="seconds item">
-          <span className="seconds number">09</span>
-          <span className="seconds word">seconds</span>
-        </li>
-      </ul>
-    </div>
+    <ul className="counter-list">
+      <li className="counter-list__item">
+        <span className="counter-list__number">02</span>
+        <span className="counter-list__word">days</span>
+      </li>
+      <li className="counter-list__item">
+        <span className="counter-list__number">20</span>
+        <span className="counter-list__word">hours</span>
+      </li>
+      <li className="counter-list__item">
+        <span className="counter-list__number">34</span>
+        <span className="counter-list__word">minutes</span>
+      </li>
+      <li className="counter-list__item">
+        <span className="counter-list__number">09</span>
+        <span className="counter-list__word">seconds</span>
+      </li>
+    </ul>
   );
 };
 
